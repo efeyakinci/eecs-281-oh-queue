421650a610640695ecfb5a6ef076337759cc3334
efeakinci <efeakinciwork@gmail.com>
2024-02-12 10:04:19 -0500

    Fix issues with reauthentication and add more input validation

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import "@/styles/globals.css";
 import {ChakraProvider} from "@chakra-ui/react";
 import {GoogleOAuthProvider} from "@react-oauth/google";
 import {GOOGLE_OAUTH_CLIENT_ID} from "@/service_components/ServiceParameters";
+import theme from "@/styles/Theme";
 
 export default function App({Component, pageProps}) {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <GoogleOAuthProvider clientId={GOOGLE_OAUTH_CLIENT_ID}>
                 <Component {...pageProps} />
             </GoogleOAuthProvider>
---
+++ styles/Theme.js
@@ -0,0 +1,9 @@
+import {extendTheme} from "@chakra-ui/react";
+
+const theme = extendTheme({
+    config: {
+        disableTransitionOnChange: false
+    }
+});
+
+export default theme;
