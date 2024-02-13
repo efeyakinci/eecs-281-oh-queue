cdb3522c06116e4627dc3e4abff3f2bd1433231b
efeakinci <efeakinciwork@gmail.com>
2024-02-13 13:46:14 -0500

    Add online indicator

--- service_components/ServiceParameters.js
+++ service_components/ServiceParameters.js
@@ -1,4 +1,4 @@
-export const SERVICE_URL = process.env.NODE_ENV === "development" && false ?
+export const SERVICE_URL = process.env.NODE_ENV === "development" ?
     "http://localhost:8080" :
     "https://eecsohservice.efeakinci.com";
 export const GOOGLE_OAUTH_CLIENT_ID = "308272798456-91fb6rtpvvtif07uaa1k67jibg17ca3b.apps.googleusercontent.com";
